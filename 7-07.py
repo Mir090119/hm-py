eed67cc9c38260df1bfd07ebe95e8171465fd3d5
Mir090119 <rahmonbek40@gmail.com>
2025-07-07 18:09:04 +0500

    07-07

---
+++ 7-07.py
@@ -0,0 +1,93 @@
+# def smash_factor ( bc , cs ):
+#     return bc / cs
+
+# print(smash_factor(134.4,93.8))
+
+# -==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+# import math
+# def distanc_home (a:list):
+#     sum = 0 
+#     for i in a :
+#         sum += i 
+#     return sum    
+
+
+# print(abs( distanc_home ([2,4,2,5])))
+
+# -==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+# def brige (b):
+#     return ' '  not in bridge
+
+# print (brige('####'))
+
+# -==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+# def programist ( a , b, c ):
+#     a = max (a,b,c)
+#     b = min (a,b,c)
+#     return (a-b)
+
+# print (programist(147,33,526))
+
+# -==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+# def color ( n , w ,h ):
+#     return n // w // h 
+
+# print (color(10,15,12)) 
+
+
+# -==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+# def nafam ( a):
+#     return 1 - a 
+
+# print(nafam(0))
+
+# -==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+# def calkulataro ( a,b,c ):
+#     if c == '+':
+#         return a + b 
+#     elif c == "-":
+#         return a - b 
+#     elif c == '*':
+#         return a * b
+#     elif c == '//':
+#         return a// b
+#     elif c == '%':
+#         return a % b 
+#     elif c == '/':
+#         return a / b 
+
+
+# print (calkulataro(4,9,'-')) 
+
+
+# def nafam (n):
+#     return n * 2 - 2
+
+# print (nafam (100))
+
+# -==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+
+# def cnt (s):
+#     return s.count("potato")
+
+# print(cnt('potatopotatopotato'))
+
+
+   
+
+
+ 
+
+
+
+
+
+
