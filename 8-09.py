d78f98b3497e977971f7370071c3ba188c89c485
Mir090119 <rahmonbek40@gmail.com>
2025-07-08 18:04:12 +0500

    08-07

---
+++ 8-09.py
@@ -0,0 +1,158 @@
+# ls = [10,20,30,40,50]
+# print ( ls[3]) Элементы доступа: Распечатать третий элемент.
+
+# print (len(ls))  Длина списка: вывести количество элементов в списке.
+
+# ls2 = len (ls)
+# if ls2 == 0 :
+#     print ( 'лист  пустой ').   Проверить, пуст ли список: Напишите код для проверки, пуст ли список.
+# else:
+#     print('лист не пустой ')
+
+# =-=-=-=-=-=---=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# ls = [10,20,30,40,50]
+# new_ls = ls[1] = 200.   Изменить элемент: измените второй элемент списка на 200 и распечатайте обновленный список.
+# print (ls)
+
+# py = ls.append(600)  элемент: добавить 600 в конец списка и вывести новый список.
+# print(ls)
+
+# pop = ls.pop(-1) 
+# print (ls)
+
+# =-=-=-=-=-=---=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+# ls = [10,20,30,40,50]
+# sum = 0 
+# for i in ls:
+#     sum += i 
+#     t = sum / len (ls).   Вычислите и выведите сумму и среднее значение всех чисел в списке.
+# print (sum)
+# print (t)
+
+# ls.reverse().     Переверните список
+# print (ls)
+
+
+# ls2 = []
+# for i in ls:
+#     ls2.append(i*i).     Дан список чисел. Напишите программу, которая преобразует каждый элемент списка в его квадрат.
+
+# print (ls2)
+
+# ls = [8, 2, 15, 1, 9]
+# print (f'максималное число{max(ls)}')
+# print (f'минималние число{min(ls)}').        Найдите и выведите наибольшее и наименьшее число в списке
+   
+
+# sports = ['Cricket', 'Football', 'Hockey', 'Football', 'Tennis','Football'].  Подсчитайте и выведите, сколько раз 'Football'это слово встречается в списке.
+# print(sports.count('Football'))
+
+
+
+# numbers = [5, 2, 8, 1, 9].  
+# numbers.sort().        Отсортируйте заданный список чисел в порядке возрастания и выведите его на печать.
+# print (numbers)
+
+
+# num = [10, 20, 30]
+# py = num.copy().    Создание копии списка
+# print (py)
+
+
+# ls1 = [1,2,3,]
+# ls2 = [4,5,6]
+# ls1.extend(ls2)
+# print(ls1)
+
+# ls =["Mike", "", "Emma", "Kelly", "", "Brad"]
+# res = list(filter(None,ls))
+# print (res)
+
+
+
+
+
+# list1 = [5, 20, 15, 20, 25, 50, 20]
+# a=list1.pop(1)
+# a=list1.remove(20)
+# a=list1.pop(-1)
+# print(list1)
+
+# list1 = [5, 20, 15, 20, 25, 50, 20]
+# for item in list1:
+#    if item == 20: 
+#       list1.remove(item)
+# print(list1)
+
+
+
+
+
+# my_list = [1, 2, 3, 'Jessa', 4, 5, 'Kelly', 'Jhon', 6]
+# a=my_list.remove('Jessa')
+# a=my_list.remove('Kelly')
+# a=my_list.remove('Jhon')
+# print(my_list)
+
+
+
+# nested_list = "background-color: initial; font-family: inherit; font-size: inherit; text-align: initial; color: initial;">[[10, 20, 30], [44, 55, 66], [77, 87, 99]]
+# a=nested_list[1][1]
+# print(a)
+# nashud
+
+
+
+# list_of_lists = [[1, 2], [3, 4], [5, 6, 7]]
+# a=list_of_lists.
+# print(list_of_lists)
+
+
+
+# list1 = ["M", "na", "i", "Ke"]
+# list2 = ["y", "me", "s", "lly"]
+# a=[]
+# for i in range(len(list1)):
+#     a.append(list1[i] + list2[i])
+# print(a)
+
+
+
+# list1 = ["Hello ", "take "]
+# list2 = ["Dear", "Sir"]
+# a = []
+# for i in list1:
+#     for j in list2:
+#         a.append(i + j)
+# print(a)
+
+
+
+# list1 = [10, 20, 30, 40]
+# list2 = [100, 200, 300, 400]
+# a = []
+# for i in range(len(list1)):
+#     a.append((list1[i], list2[i]))
+# print(a)
+
+
+
+# list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+# list1[2][2].append(7000)
+# print(list1)
+
+
+# list1 = [5, 10, 15, 20, 25, 50, 20]
+# index= list1.index(20)
+# list1[index]=200
+# print(list1)
+
+
+# =-=-=-=-=-=---=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+
+
+
+
+
