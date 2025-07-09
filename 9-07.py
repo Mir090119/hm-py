9c525fc25e60affef67884c1aa540cb0b92fafd8
Mir090119 <rahmonbek40@gmail.com>
2025-07-09 18:03:14 +0500

    0907

---
+++ 9-07.py
@@ -0,0 +1,128 @@
+# mydict={
+#     'Alisa':80,
+#     'Bob':78,
+#     'Charlie': 90.        add and change to tuple 
+# }
+# mydict ['color '] = 20
+# mydict ['Bob']= 95
+# print (mydict)
+
+# mydict = {
+#     'nobuk':1500,
+#     'smartfon':1200,
+#     'naushnik':300,     max values  for tuple
+#     'monitor':700      
+# }
+
+# print ( max( mydict.values()))
+
+
+
+# 1.  =-0-0-=-=-=-=-=-=-=-=-=-====-=-=-=-=-=-=-=-=--=-==-==-===-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+# my_dict = {
+#     'name': 'Alice',
+#     'age': 35,
+#     'city': 'new york'
+# }
+# my_dict['profesiy'] = 'doctor '
+# print (my_dict)
+# my_dict [ 'age'] = 40
+# print (my_dict)
+
+# print(my_dict.get('city'))
+
+
+#  2.   =-0-0-=-=-=-=-=-=-=-=-=-====-=-=-=-=-=-=-=-=--=-==-==-===-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# my_dict = {
+#     'name': 'Alice',
+#     'age': 35,
+#     'city': 'new york',
+#     'profesiya':' doctor'
+# }
+# my_dict.pop('profesiya')
+# print( my_dict )
+# print( my_dict.items())
+# print ('age'in my_dict)
+
+# 3.  =-0-0-=-=-=-=-=-=-=-=-=-====-=-=-=-=-=-=-=-=--=-==-==-===-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+# dicti = {}
+# keys = [ 'Ten', 'Twenty', 'Thirty']
+# values = [10, 20, 30]
+# for i in range (len (keys)):
+#     key = keys [i]
+#     value = values[i]
+#     dicti [key] = value
+
+# print(dicti)
+
+# 4.  =-0-0-=-=-=-=-=-=-=-=-=-====-=-=-=-=-=-=-=-=--=-==-==-===-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+# my_dict = {'name': 'Alice', 'age': 35, 'city': 'New York'}
+# my_dict.clear()
+# print (my_dict)
+
+
+# 5.  =-0-0-=-=-=-=-=-=-=-=-=-====-=-=-=-=-=-=-=-=--=-==-==-===-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+# dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+# dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+# dict1.update( dict2) 
+# print (dict1)
+
+
+# 6.  =-0-0-=-=-=-=-=-=-=-=-=-====-=-=-=-=-=-=-=-=--=-==-==-===-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+# string1 = 'Jessa'
+# dicti =  {}
+# for i in string1 :
+#     if i not in dicti : 
+#         dicti[i] = 1 
+#     else:
+#         dicti[i]+=1
+
+# print(dicti)
+
+# 7.  =-0-0-=-=-=-=-=-=-=-=-=-====-=-=-=-=-=-=-=-=--=-==-==-===-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    
+
+
+# data = {'person': {'name': 'Alice', 'age': 30}}
+# t = data ['person']['age']
+# print(t)
+
+# 8.  =-0-0-=-=-=-=-=-=-=-=-=-====-=-=-=-=-=-=-=-=--=-==-==-===-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+
+# dict = {
+#     "class": {
+#         "student": {
+#             "name": "Mike",
+#             "marks": {
+#                 "physics": 70,
+#                 "history": 80
+#             }
+#         }
+#     }
+# }
+
+# print(dict['class']['student']['marks']['history'])
+
+# 9.  =-0-0-=-=-=-=-=-=-=-=-=-====-=-=-=-=-=-=-=-=--=-==-==-===-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# employees = ['Kelly', 'Emma']
+# defaults = {"designation": 'Developer', "salary": 8000}
+# mydicti = {}
+# for i in  employees :
+#     mydicti[i] = defaults
+#     print (mydicti)
+
+
+
+
+
+
+
+
+
+
